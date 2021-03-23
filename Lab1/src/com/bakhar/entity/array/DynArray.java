f293335e95510d5a19f7565017639178a98fd352
Artem Bakhar <baxar2001@gmail.com>
2021-03-23 23:36:50 +0300

    lab1

---
+++ Lab1/src/com/bakhar/entity/array/DynArray.java
@@ -0,0 +1,71 @@
+package com.bakhar.entity.array;
+
+
+public class DynArray {
+    private int size;
+    private int capacity;
+    private double[] data;
+
+    public int getSize() {
+        return size;
+    }
+
+    public DynArray() {
+        size = 0;
+        capacity = 0;
+        data = null;
+    }
+
+    public DynArray(int size) {
+        this.size = size;
+        this.capacity = size;
+        data = new double[capacity];
+    }
+
+    public void add(double x) {
+        if (size == capacity) {
+            resize();
+        }
+        data[size] = x;
+        size++;
+    }
+
+    public double getAt(int pos) throws DynArrayException {
+        if (pos >= size || pos < 0) {
+            throw new DynArrayException("Wrong pos!");
+        }
+        return data[pos];
+    }
+
+    public void eraseAt(int pos) throws DynArrayException {
+        if (size == 0) {
+            throw new DynArrayException("No Elements");
+        }
+        if (pos >= size || pos < 0) {
+            throw new DynArrayException("Wrong pos!");
+        }
+        for (int i = pos; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+    }
+
+    private void resize() {
+        this.capacity = (this.capacity+1) * 2;
+        double[] tempData = new double[this.capacity];
+        for (int i = 0; i < size ; i++) {
+            tempData[i] = data[i];
+        }
+        data = tempData;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append(data[i]).append(" ");
+        }
+        stringBuilder.append("\n");
+        return stringBuilder.toString();
+    }
+}
---
+++ Lab1/src/com/bakhar/entity/array/DynArrayException.java
@@ -0,0 +1,7 @@
+package com.bakhar.entity.array;
+
+public class DynArrayException extends Exception{
+    public DynArrayException(String message) {
+        super(message);
+    }
+}
