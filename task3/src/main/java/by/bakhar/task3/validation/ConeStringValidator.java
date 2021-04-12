2fd6ccf606a0971439149391b30771bb24c88597
Artem Bakhar <baxar2001@gmail.com>
2021-04-12 18:15:47 +0300

    Reg

--- task3/src/main/java/by/bakhar/task3/validation/ConeStringValidator.java
+++ task3/src/main/java/by/bakhar/task3/validation/ConeStringValidator.java
@@ -1,7 +1,7 @@
 package by.bakhar.task3.validation;
 
 public class ConeStringValidator {
-    private final static String DOUBLE_REGEX = "^\\d+(\\.\\d+)?\\s+\\d+(\\.\\d+)?\\d+(\\.\\d+)?\\s+\\d+(\\.\\d+)?\\s+\\d+(\\.\\d+)?\\s+\\d+(\\.\\d+)?\\s+\\d+(\\.\\d+)?\\s+";
+    private static final String DOUBLE_REGEX = "^((-)?\\d+(\\.\\d+)?\\s){6}(-)?\\d+(\\.\\d+)?";
 
     public static boolean isStringValid(String data) {
         boolean valid = data.matches(DOUBLE_REGEX);
