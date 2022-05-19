f6981845fd370d5066cff577a8e9bd556439d58a
Artem <baxar2001@gmail.com>
2022-05-19 13:17:49 +0300

    part2

---
+++ src/main/java/bakhar/server/bsu/Server.java
@@ -0,0 +1,58 @@
+package bakhar.server.bsu;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Server {
+    static final int PORT = 8080;
+    private List<ClientHandler> clients = new ArrayList<>();
+    private HashMap<Integer, String> information = new HashMap<>();
+
+
+    public Server() {
+        Socket clientSocket = null;
+        ServerSocket serverSocket = null;
+        try {
+            // создаём серверный сокет на определенном порту
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+            while (true) {
+                clientSocket = serverSocket.accept();
+                ClientHandler client = new ClientHandler(clientSocket, this);
+                clients.add(client);
+                new Thread(client).start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+                System.out.println("Сервер остановлен");
+                serverSocket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessageToAllClients(String msg) {
+        for (ClientHandler o : clients) {
+            o.sendMsg(msg);
+        }
+
+    }
+
+    public void removeClient(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public HashMap<Integer, String> getInformation() {
+        return information;
+    }
+
+}
---
+++ src/main/java/bakhar/server/bsu/ServerMain.java
@@ -0,0 +1,7 @@
+package bakhar.server.bsu;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
